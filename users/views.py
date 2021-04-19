9f03e41505d45a497d05738b40767afec87f4e4a
keunho_kim <astrocat9311@gmail.com>
2021-04-20 01:51:52 +0900

    users views.py 수정 요청사항 반영

--- users/views.py
+++ users/views.py
@@ -2,55 +2,61 @@ import json, traceback
 import bcrypt
 import jwt
 
-from django.http            import JsonResponse
+from django.http import JsonResponse
 from django.core.exceptions import ValidationError
-from django.views           import View
-from django.db.models       import Q
+from django.views import View
+from django.db.models import Q
 
 from users.models import User
-from users.validations      import validate_email, validate_phone, validate_password, validate_account
-
-from users.models import User
+from users.validations import validate_email, validate_phone, validate_password, validate_account
 
 
 class SignUpView(View):
     def post(self, request):
-        data = json.loads(request.body)
+        data         = json.loads(request.body)
+        account      = data['account']
+        password     = data['password']
+        email        = data['email']
+        name         = data['name']
+        phone_number = data['phone_number']
+        birthday     = data['birthday']
+
 
         try:
-            if User.objects.filter(Q(account=data["account"]) |
-                                   Q(email=data['email'])
+            if User.objects.filter(Q(account=account) |
+                                   Q(email=email)
                                    ).exists():
                 return JsonResponse({'message': 'ACCOUNT_EXISTS'})
 
-            if not data['account'] or not data['password'] or \
-                    not data['name'] or not data['email'] or not data['phone_number']:
+            if not account or not password or \
+                    not name or not email or not phone_number:
                 return JsonResponse({'message': 'NO_NECESSARY_VALUES'}, status=400)
 
-            if not validate_account(data['account']):
+            if not validate_account(account):
                 return JsonResponse({'message': 'USE_VALID_ACCOUNT'}, status=400)
 
-            if not validate_password(data['password']):
-                return JsonResponse({'message': 'USE_VALID_PASSWORD'},status=400)
+            if not validate_password(password):
+                return JsonResponse({'message': 'USE_VALID_PASSWORD'}, status=400)
 
-            if not validate_email(data['email']):
-                return JsonResponse({'message': 'USE_VALID_EMAIL'},status=400)
+            if not validate_email(email):
+                return JsonResponse({'message': 'USE_VALID_EMAIL'}, status=400)
 
-            if not validate_phone(data['phone_number']):
-                return JsonResponse({'message': 'USE_VALID_NUMBER'},status=400)
+            if not validate_phone(phone_number):
+                return JsonResponse({'message': 'USE_VALID_NUMBER'}, status=400)
 
-            hashed_password = bcrypt.hashpw(data['password'].encode('UTF-8'), bcrypt.gensalt()).decode()
+            hashed_password = bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt()).decode()
 
             User.objects.create(
-                account      = data['account'],
-                phone_number = data['phone_number'],
-                name         = data['name'],
-                email        = data['email'],
-                birthday     = data.get('birthday', 'not available'),
-                address      = data.get('address', 'not available'),
+                account      = account,
+                password     = hashed_password,
+                phone_number = phone_number,
+                name         = name,
+                email        = email,
+                birthday     = birthday,
+                address      = data.get('address', null),
                 mileage      = data.get('mileage', 3500),
             )
-            return JsonResponse({'message': 'SUCCESS', "user_account": data['account']}, status=201)
+            return JsonResponse({'message': 'SUCCESS', "user_account": account}, status=201)
 
         except ValidationError:
             return JsonResponse({'message': 'VALIDATION_ERROR'}, status=400)
@@ -65,18 +71,21 @@ class LogInView(View):
         try:
             data = json.loads(request.body)
 
-            if not validate_account(data['account']):
-                return JsonResponse({'message':'WRONG_ACCOUNT'}, status=400)
+            account  = data['account']
+            password = data['password']
 
-            if not User.objects.filter(account=data['account']).exists():
+            if not validate_account(account):
+                return JsonResponse({'message': 'WRONG_ACCOUNT'}, status=400)
+
+            if not User.objects.filter(account=account).exists():
                 return JsonResponse({'message': 'ACCOUNT_NOT_EXIST'}, status=401)
 
-            user_account = User.objects.get(account=data['account'])
-            if not bcrypt.checkpw(data['password'].encode('utf-8'), user_account.password.encode('utf-8')):
+            user_account = User.objects.get(account=account)
+            if not bcrypt.checkpw(password.encode('utf-8'), user_account.password.encode('utf-8')):
                 return JsonResponse({'message': 'INCORRECT_PASSWORD'}, status=400)
 
             access_token = jwt.encode({'id': user_account.id}, 'secret', algorithm='HS256')
-            return JsonResponse({'token': access_token, 'message': 'SUCCESS', 'user_account': data['account']},
+            return JsonResponse({'token': access_token, 'message': 'SUCCESS', 'user_account': account},
                                 status=200)
         except ValidationError:
             return JsonResponse({'message': 'VALIDATION_ERROR'}, status=400)
