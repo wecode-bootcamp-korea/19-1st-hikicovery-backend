457554c3dfb5e078f8116abcc1abcc3a4a96af99
keunho_kim <astrocat9311@gmail.com>
2021-04-19 17:19:15 +0900

    상품 후기 작성 및 조회 기능 구현

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from django.http            import JsonResponse
+from django.core.exceptions import ValidationError
+from django.views           import View
+from django.db.models       import Q
+from products.models        import Product,ProductDetail
+from carts.models           import ProductDetailOrder,Order
+from users.models           import User
+from models                 import Review,ReviewPhoto,WishList
+
+class ReviewCreateView(View):
+    def post(self,request):
+        data=json.loads(request.body)
+
+        review = Review.objects.create(
+                 rating         = data['rating'],
+                 comment        = data['comment'],
+                 color_review   = data['color_review'],
+                 size_review    = data['size_review'],
+                 product_detail = ProductDetail.objects.get(product_id=data['product_id'], size_id=data['size_id']).id
+                 )
+
+        ReviewPhoto.objects.create(
+                 image_url=data.get('image_url', None),
+                 review_id=review.id
+                 )
+
+class ReviewShowView(View):
+    def get(self,request):
+        data=json.loads(request.body)
+
+        if ProductDetail.objects.get(product__name=data['product']).exists():
+            review_photo=[photo for photo in ReviewPhoto.objects.filter(review_id=data['review_id'])]
+
+            data = {
+                'rating'        : Review.rating,
+                'comment'       : Review.comment,
+                'color_review'  : Review.color_review,
+                'size_review'   : Review.size_review,
+                'create_at'     : Review.create_at,
+                'update_at'     : Review.update_at,
+                'product_detail': Review.product_detail,
+                'review_photo'  : review_photo.image_url,
+                'size'          : ProductDetail.size_id,
+                'color'         : Product.color_id
+            }
+        return JsonResponse({'data': json.dumps(data)}, status=200)
+
+class WishListCreateView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+
+        WishList.objects.create(
+            user_id    = User.objects.get(user_id = User.id),
+            product_id = Product.objects.get(product_id = Product.id)
+        )
+
+class WishListShowView(View):
+    def get(self,request):
+        data = json.loads(request.body)
+
+        return JsonResponse({'data': json.dumps(data)}, status=200)
+
