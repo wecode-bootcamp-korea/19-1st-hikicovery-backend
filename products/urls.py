d1cabe83469935cd05b3399d2938e0e539fd651b
audwls624 <78643271+audwls624@users.noreply.github.com>
2021-04-21 16:47:04 +0900

    products app url 오타 수정

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from products.views import ProductDetailView, BestItemView, ProductView
 
 urlpatterns = [
     path('', ProductView.as_view()),
-    path('/best-itmes', BestItemView.as_view()),
+    path('/best-items', BestItemView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
 ]
