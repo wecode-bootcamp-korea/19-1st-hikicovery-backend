1b26e4bfaf0ee72161152e7089aae3476f54115c
keunho_kim <astrocat9311@gmail.com>
2021-04-22 19:56:41 +0900

    데코레이터 수정

--- utils.py
+++ utils.py
@@ -10,12 +10,12 @@ def login_required(func):
             token = jwt.decode(access_token, SECRET_KEY, algorithms=algorithm)
             user = User.objects.filter(id=token['id'])
             if user.exists():
-                request.user = user
+                request.user = User.objects.get(id=token['id'])
                 return func(self, request, *args, **kwargs)
+            return JsonResponse({"message": "UNKNOWN_USER"}, status=401)
         except KeyError:
             return JsonResponse({"message": "INVALID_LOGIN"}, status=401)
         except jwt.DecodeError:
             return JsonResponse({"message": "INVALID_TOKEN"}, status=401)
-        return JsonResponse({"message": "UNKNOWN_USER"}, status=401)
 
     return decorator
